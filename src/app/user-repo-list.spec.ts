c0254aa3e36308c767e8920e595da8d7e4221147
beli302 <belindashirkiz2@gmail.com>
2020-01-19 17:29:26 +0300

    added class user-repo-list

---
+++ src/app/user-repo-list.spec.ts
@@ -0,0 +1,7 @@
+import { UserRepoList } from './user-repo-list';
+
+describe('UserRepoList', () => {
+  it('should create an instance', () => {
+    expect(new UserRepoList()).toBeTruthy();
+  });
+});
